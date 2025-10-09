3ec0bc2d49906265ec7d4127f8a005ae2e2c417e
Bahoz <julienbraultcherif@gmail.com>
2025-10-09 23:34:42 +0200

    fix problems

--- app/components/ListTiktok.tsx
+++ app/components/ListTiktok.tsx
@@ -2,14 +2,14 @@
 
 import { Video } from "@/types";
 import React, { useEffect, useState } from "react";
-import { AiFillTikTok } from "react-icons/ai";
+import { AiFillTikTok, AiOutlineLoading } from "react-icons/ai";
 import { FaCheck, FaHourglassStart } from "react-icons/fa";
 import { RxCross1 } from "react-icons/rx";
 
 export default function ListTiktok() {
   const [videos, setVideos] = useState<Video[]>([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<string | null>(null);
   useEffect(() => {
     const fetchVideos = async () => {
       try {
@@ -21,52 +21,63 @@ export default function ListTiktok() {
         );
         setVideos(sortedData);
         setLoading(false);
-      } catch (error: any) {
-        setError(error.message);
+      } catch (error: unknown) {
+        if (error instanceof Error) setError(error.message);
+        else setError(String(error));
       }
     };
     fetchVideos();
   }, []);
   return (
-    <div className="text-white flex flex-col gap-8 overflow-y-scroll max-h-[70dvh]">
-      {videos.map((v: Video) => (
-        <div
-          className="bg-gray-900 flex items-center justify-center py-6 px-8 gap-12 rounded-3xl"
-          key={v.id}
-        >
-          {v.type === "tiktok" && <AiFillTikTok size={48} />}
-          <div className="flex flex-col items-start gap-3">
-            <h3 className="text-lg">{v.prompt.slice(0, 100)}</h3>
-            <h4 className="text-sm">
-              <span className="underline">OperationID :</span> {v.operationId}
-            </h4>
-            {v.uri && (
-              <h5 className="text-sm">
-                <span className="underline">URI :</span> {v.uri}
-              </h5>
-            )}
-            <h6 className="text-xs">{v.createdAt}</h6>
-          </div>
-          <div className="flex gap-6">
-            <div>
-              <h4 className="text-sm mb-3">État génération : </h4>
-              {v.done ? (
-                <FaCheck className="text-green-400" size={32} />
-              ) : (
-                <FaHourglassStart className="text-orange-300" size={32} />
+    <div
+      className={`text-white flex flex-col gap-8 max-h-[70dvh] ${
+        !loading && "overflow-auto"
+      }`}
+    >
+      {!loading ? (
+        videos.map((v: Video) => (
+          <div
+            className="bg-gray-900 flex items-center justify-center py-6 px-8 gap-12 rounded-3xl"
+            key={v.id}
+          >
+            {v.type === "tiktok" && <AiFillTikTok size={48} />}
+            <div className="flex flex-col items-start gap-3">
+              <h3 className="text-lg">{v.prompt.slice(0, 100)}</h3>
+              <h4 className="text-sm">
+                <span className="underline">OperationID :</span> {v.operationId}
+              </h4>
+              {v.uri && (
+                <h5 className="text-sm">
+                  <span className="underline">URI :</span> {v.uri}
+                </h5>
               )}
+              <h6 className="text-xs">{v.createdAt}</h6>
             </div>
-            <div>
-              <h4 className="text-sm mb-3">État du post : </h4>
-              {v.isPosted ? (
-                <FaCheck size={32} />
-              ) : (
-                <RxCross1 className="text-red-400" size={32} />
-              )}
+            <div className="flex gap-6">
+              <div>
+                <h4 className="text-sm mb-3">État génération : </h4>
+                {v.done ? (
+                  <FaCheck className="text-green-400" size={32} />
+                ) : (
+                  <FaHourglassStart className="text-orange-300" size={32} />
+                )}
+              </div>
+              <div>
+                <h4 className="text-sm mb-3">État du post : </h4>
+                {v.isPosted ? (
+                  <FaCheck size={32} />
+                ) : (
+                  <RxCross1 className="text-red-400" size={32} />
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : error ? (
+        <h2>Une erreur est survenue : {error}</h2>
+      ) : (
+        <AiOutlineLoading size={32} className="text-black animate-spin" />
+      )}
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
-import GenerateVideoButton from "./components/GenerateVideoButton";
-import TiktokButton from "./components/TiktokButton";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
